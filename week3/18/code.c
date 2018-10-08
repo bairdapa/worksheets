61be669e0260887149f0049776b65ed1a8f94ea9
bairdapa <35271333+bairdapa@users.noreply.github.com>
2018-10-08 14:39:04 -0700

    added code questions

--- week3/18/code.c
+++ week3/18/code.c
@@ -1,1 +1,32 @@
+struct link {
+  TYPE value;
+  struct link * next;
+};
 
+struct listQueue {
+  struct link *firstLink;
+  struct link *lastLink;
+};
+
+void listQueueInit (struct listQueue *q) {
+  struct link *lnk = (struct link *) malloc(sizeof(struct link));
+  assert(lnk != 0); /* lnk is the sentinel */
+  lnk->next = 0;
+  q->firstLink = q->lastLink = lnk;
+}
+
+void listQueueAddBack (struct listQueue *q, TYPE e) {
+
+}
+
+TYPE listQueueFront (struct listQueue *q) {
+
+}
+
+void listQueueRemoveFront (struct listQueue *q) {
+
+}
+
+int listQueueIsEmpty (struct listQueue *q) {
+
+}
