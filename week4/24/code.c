06e758d606f3ced6e765c2b50b0420cca7527146
estheripark <44016527+estheripark@users.noreply.github.com>
2018-10-21 10:09:05 -0700

    Update code.c

---
+++ week4/24/code.c
@@ -0,0 +1,42 @@
+int linkedListIteratorHasNext (struct linkedListIterator *itr) {
+
+
+
+
+
+
+
+
+}
+
+TYPE linkedListIteratorNext (struct linkedListIterator *itr) {
+
+
+
+
+
+
+
+
+
+}
+
+/* think about the next one carefully. Draw a picture. What should itr->currentLink be pointing to after the remove operation? */
+
+void linkedListIteratorRemove (struct linkedListIterator *itr) {
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
