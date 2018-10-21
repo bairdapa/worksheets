5e20c9e7455dc2e8d20868db9aa4a359941d696f
falexh <hellardf@oregonstate.edu>
2018-10-21 10:05:55 -0700

    added assertions, fixed hasNext

--- week4/23/code.c
+++ week4/23/code.c
@@ -1,26 +1,29 @@
 void dynArrayIteratorInit (struct dynArray *da, struct dynArrayIterator *itr) {
+    assert(da != 0);
+    assert(itr != 0);
     itr->da = da;
     itr->currentIndex = 0;
 }
 
 
 int dynArrayIteratorHasNext (struct dynArrayIterator *itr) {
+    assert(itr != 0);
     if (itr->currentIndex < itr->da->size)
     {
-	    itr->currentIndex++;
       return 1;
     }
-return 0;
+      return 0;
 }
 
 
 TYPE dynArrayIteratorNext (struct dynArrayIterator *itr) {
    	assert(DynArrIteratorHasNext(itr));
-	return itr->da->data[itr->currentIndex];
+	return itr->da->data[itr->currentIndex++];
 }
 
 
-void dynArrayIteratorRemove (struct dynArrayIterator *itr) {	
+void dynArrayIteratorRemove (struct dynArrayIterator *itr) {
+	assert(itr != 0);
   	dynArrayRemoveAt (itr->da, itr->currentIndex);
  	itr->currentIndex--;
 }
