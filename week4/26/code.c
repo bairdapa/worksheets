c7e3d5f3cd338ed3716e0005876956b1134accec
estheripark <44016527+estheripark@users.noreply.github.com>
2018-10-21 09:55:41 -0700

    Update code.c

---
+++ week4/26/code.c
@@ -0,0 +1,15 @@
+1.	What is the algorithmic complexity of the binary search algorithm?
+ O(log n)
+ 
+2.	Using your answer to the previous question, what is the algorithmic complex of the method contains for an OrderedArrayBag?
+O(log n)
+
+3.	What is the algorithmic complexity of the method addAt?
+ O(n), since there is extra work to loop through every element in the array.
+ 
+4.	Using your answer to the previous question, what is the algorithmic complexity of the method add for an OrderedArrayBag?
+The binary search algorithm has O(log n). The addAt function as O(n) complexity which dominates O(log n). The function therefore would have O(n) complexity.
+
+5.	What is the complexity of the method removeAt? of the method remove?
+removeAt uses a binary search algorithm which is O(log n) complexity, but will be dominated by the O(n) operation when the removed element position is filled. 
+The function would therefore be O(n). complexity.
