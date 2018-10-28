f96bcbe7b7733f588909c23dee19495a5c09b0c4
estheripark <44016527+estheripark@users.noreply.github.com>
2018-10-28 10:39:42 -0700

    Update code.c

--- week5/29/code.c
+++ week5/29/code.c
@@ -13,9 +13,41 @@ int sizeBST (struct BinarySearchTree *tree) {
 }
 	
 struct Node * _nodeAddBST (struct Node *current, TYPE newValue) {
+	assert(current != NULL);
+	struct Node *newNode;
+	
+	if(current == 0)
+	{
+		newNode = malloc(sizeof(struct Node));
+		assert(newNode != 0);
+		newNode->value = value;
+		newNode->left = newNode->right = 0;
+		return newNode;	
+	}
+	else if(value < current->value) 
+	{
+		current->left = _addNode(current->left, value);
+	}
+	else 
+	{ 
+		current->right = _addNode(current->right, value);
+	}
+
+	return current;
 }
 
 int containsBST (struct BinarySearchTree *tree, TYPE d) {
+	struct Node *current = tree->root;
+	
+	while(current != 0)
+	{
+		if(current->value == value) 
+			return 1;
+		else if(value < current->value) 
+			current = current->left;
+		else
+			current = current-right;
+	}
 }
 
 void removeBST (struct BinarySearchTree *tree, TYPE d) {
@@ -26,10 +58,56 @@ void removeBST (struct BinarySearchTree *tree, TYPE d) {
 }
 
 TYPE _leftMostChild (struct Node * current) {
+	assert(current != 0);
+	while(current->left != 0)
+	{
+		current = current->left;
+	}
+	
+	return current->value;
 }
 
 struct node * _removeLeftmostChild (struct Node *current) {
+	assert(current != 0)
+	
+	struct Node *temp = current->right;
+
+	if(current->left == 0) 
+	{
+		free(current);
+		return temp;
+	} 
+	else 
+	{ 
+		current->left = _removeLeftMostChild(current->left);
+	}
+
+	return current;
 }
 
 struct Node * _nodeRemoveBST (struct Node * current, TYPE d) {
+	if(current->value == value) 
+	{
+		if(current->right = 0) 
+		{	
+			struct node *temp = current->left;
+			free(current);
+			return temp;
+		}
+		else 
+		{
+			current->value = _leftMostChild(current->right);
+			current->right = _removeLeftMostChild(current->right);
+		}
+	} 
+	else if (value < current->value) 
+	{
+		current->left = _nodeRemoveBST(current->left, value);
+	} 
+	else 
+	{ 
+		current->right = _nodeRemoveBST(current->right, value);
+	}
+
+	return current;
 }
