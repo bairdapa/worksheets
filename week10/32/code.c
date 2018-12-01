2f5f19068cab53bc227358d619d380bfa9a6caed
bairdapa <35271333+bairdapa@users.noreply.github.com>
2018-11-30 20:07:47 -0800

    Update code.c

--- week10/32/code.c
+++ week10/32/code.c
@@ -4,5 +4,8 @@ void treeSort (TYPE *data, int n) { /* sort values in array data */
 
   for(int i = 0; i < n; i++) AVLtreeAdd(&tree, data[i]);
   
-
-} 
+  AVLIterator itr;
+  AVLIteratorInit(tree, &itr);
+  
+  for(int i = 0; i < n; i++) data[i] = AVLIteratorNext(&itr);
+}
